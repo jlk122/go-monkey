e565923e0728403b661cc6106b45a930152b31b0
Lukas Karlsson <johnlukas.k@gmail.com>
2024-04-12 22:54:23 +0200

    added TokenType constants

--- token/token.go
+++ token/token.go
@@ -7,4 +7,28 @@ type Token struct {
 	Literal string
 }
 
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
+	// Identifiers + literals
+	IDENT = "IDENT"	// add, foobvar, x, y
+	INT   = "INT"	
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)

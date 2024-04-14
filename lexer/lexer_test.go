f66b119cd67ef38aa9ef8d9c76f3162b09d3510f
Lukas Karlsson <johnlukas.k@gmail.com>
2024-04-14 19:14:36 +0200

    Added test case with int, keyword, function def and call

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,33 +1,19 @@
 package lexer
 
 import (
-	"testing"
 	"monkey/token"
+	"testing"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+type TokenTest struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
 
-	// []type{<elements>} - slice type = dynamic array kinda?
-	// In tests that mean [] -> dynamic size, struct{eT, eL} -> type ,{{token.ASSIGN, "="},{} etc} -> elements
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
+func compareStringWithTokenList(t *testing.T, input string, test_tokens []TokenTest) {
 	l := New(input)
 
-	for i, tt := range tests {
+	for i, tt := range test_tokens {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
@@ -40,4 +26,85 @@ func TestNextToken(t *testing.T) {
 				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
+}
+
+func TestNextTokenStringSymbols(t *testing.T) {
+	input := `=+(){},;`
+
+	// []type{<elements>} - slice type = dynamic array kinda?
+	// In test that mean [] -> dynamic size, struct{eT, eL} -> type ,{{token.ASSIGN, "="},{} etc} -> elements
+	test := []TokenTest{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	compareStringWithTokenList(t, input, test)
+}
+
+func TestNextTokenStringSampleCode(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+
+	let add = fn(x, y) {
+	  x + y;
+	};
+
+	let result = add(five, ten);
+	`
+
+	test := []TokenTest{
+		// let five = 5;
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		// let ten = 10;
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+
+		// let add = fn(x,y)
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+
+		// { x + y; };
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+
+		// let result = add(five,ten)
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+	}
+
+	compareStringWithTokenList(t, input, test)
 }
